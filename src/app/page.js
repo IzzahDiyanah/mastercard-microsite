7a01326939dbf6126fd46df95588c995f972317f
Amir <amir.fikri@kingdomdigital.com.my>
2025-06-13 14:49:57 +0800

    second-section-item

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { gsap } from '../lib/gsap';
 import Hero from '../component/Hero';
+import SecondSection from '@/component/second-section';
 
 
 export default function Home() {
   return (
 <>
 <Hero />
+<SecondSection />
 {/* start adding your component below */}
 </>  
   );
